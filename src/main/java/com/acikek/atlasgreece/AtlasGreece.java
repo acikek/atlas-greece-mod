9188105a4bee6a155df080ec2f8481ecd92f8103
acikek <spiralixp@gmail.com>
2023-01-23 10:10:15 -0800

    update to 1.19.3 and finish teleporting functionality

--- src/main/java/com/acikek/atlasgreece/AtlasGreece.java
+++ src/main/java/com/acikek/atlasgreece/AtlasGreece.java
@@ -8,10 +8,10 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 import net.minecraft.block.Blocks;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -23,7 +23,7 @@ public class AtlasGreece implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger(ID);
 
     public static final Identifier DIMENSION_ID = id("greece");
-    public static final RegistryKey<World> DIMENSION = RegistryKey.of(Registry.WORLD_KEY, DIMENSION_ID);
+    public static final RegistryKey<World> DIMENSION = RegistryKey.of(RegistryKeys.WORLD, DIMENSION_ID);
 
     public static Identifier id(String path) {
         return new Identifier(ID, path);
--- src/main/java/com/acikek/atlasgreece/item/MoussakaItem.java
+++ src/main/java/com/acikek/atlasgreece/item/MoussakaItem.java
@@ -1,22 +1,24 @@
 package com.acikek.atlasgreece.item;
 
 import com.acikek.atlasgreece.AtlasGreece;
+import com.acikek.atlasgreece.Destination;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
-public class MoussakaItem extends Item {
+import java.util.Map;
 
-    public static final Item INSTANCE = new MoussakaItem();
+public class MoussakaItem extends Item {
 
     public static final FoodComponent FOOD_COMPONENT = new FoodComponent.Builder()
             .hunger(10)
@@ -25,32 +27,47 @@ public class MoussakaItem extends Item {
             .meat()
             .build();
 
-    public static final Settings SETTINGS = new FabricItemSettings()
-            .group(ItemGroup.FOOD)
-            .food(FOOD_COMPONENT);
+    public static final Settings SETTINGS = new FabricItemSettings().food(FOOD_COMPONENT);
+
+    public static final Item INSTANCE = new MoussakaItem();
 
     public MoussakaItem() {
         super(SETTINGS);
     }
 
-    public static BlockPos getTeleportPosition(World dimension, ServerPlayerEntity player) {
-        return player.getSpawnPointDimension().getValue().equals(AtlasGreece.DIMENSION_ID)
+    public static BlockPos getReturnPosition(ServerPlayerEntity player) {
+        World overworld = player.server.getOverworld();
+        return player.getSpawnPointDimension() == World.OVERWORLD && player.getSpawnPointPosition() != null
                 ? player.getSpawnPointPosition()
-                : dimension.getSpawnPos();
+                : overworld.getSpawnPos();
+    }
+
+    public static void teleport(ServerPlayerEntity player, BlockPos blockPos, RegistryKey<World> dimension) {
+        ServerWorld world = player.server.getWorld(dimension);
+        Vec3d pos = Vec3d.ofBottomCenter(blockPos);
+        player.teleport(world, pos.x, pos.y, pos.z, player.getYaw(), player.getPitch());
     }
 
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        if (!AtlasGreece.usePortal && world instanceof ServerWorld serverWorld && user instanceof ServerPlayerEntity serverPlayer) {
-            ServerWorld dimension = serverWorld.getServer().getWorld(AtlasGreece.DIMENSION);
-            BlockPos teleportPos = getTeleportPosition(dimension, serverPlayer);
-            Vec3d pos = Vec3d.ofBottomCenter(teleportPos);
-            serverPlayer.teleport(dimension, pos.x, pos.y, pos.z, serverPlayer.getYaw(), serverPlayer.getPitch());
+        if (user instanceof ServerPlayerEntity serverPlayer) {
+            System.out.println(serverPlayer.getWorld().getRegistryKey().getValue());
+            if (serverPlayer.getWorld().getRegistryKey().getValue().equals(AtlasGreece.DIMENSION_ID)) {
+                teleport(serverPlayer, getReturnPosition(serverPlayer), World.OVERWORLD);
+            }
+            else {
+                Map.Entry<String, Destination> destination = Destination.getDestination(serverPlayer);
+                teleport(serverPlayer, destination.getValue().pos(), AtlasGreece.DIMENSION);
+                serverPlayer.sendMessage(destination.getValue().getSpawnText(destination.getKey()).styled(style -> style.withItalic(true).withFormatting(Formatting.GRAY)));
+            }
         }
         return super.finishUsing(stack, world, user);
     }
 
     public static void register() {
-        Registry.register(Registry.ITEM, AtlasGreece.id("moussaka"), INSTANCE);
+        Registry.register(Registries.ITEM, AtlasGreece.id("moussaka"), INSTANCE);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries ->
+            entries.addAfter(Items.COOKED_RABBIT, INSTANCE);
+        );
     }
 }
